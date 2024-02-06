9b360290c4c47f2c95cd0b158038ce9fadd2bb19
JanuszBorko <158299519+JanuszBorko@users.noreply.github.com>
2024-02-06 18:00:13 +0100

    Add files via upload

---
+++ while.py
@@ -0,0 +1,26 @@
+print('hola')
+# while True:
+#     pass
+
+# while True:
+#     print('hola')
+
+# i = 0
+# while i<10:
+#     print(f"i con valor {i} es menos que 10")
+#     i+=1
+# print('fin de programa')
+
+# i = 0
+# while i<6:
+#     i += 1
+#     if i == 3:
+#         break
+#     print(i)
+#     if i == 5:
+#         break
+#     print(i)
+    
+for i in range(100):
+    if i not in (10:100):
+        print(i)
