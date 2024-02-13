7e6514bf1450cf9db02e1b390f01e2b3d7d72b86
Janusz <ja.borkowski@aulanz.net>
2024-02-13 16:41:59 +0100

    ok

---
+++ eliza.py
@@ -0,0 +1,8 @@
+conversacion = {}
+preg = "Hola, soy Eliza. Como te llamas?"
+print(preg)
+resp = input()
+
+conversacion.update({preg:resp})
+
+
--- test.py
+++ test.py
@@ -89,14 +89,57 @@
 # if "HOLA" in s.upper():
 #     print("si")
 
-s = "hola,Agur,Hi,Gye,goodbye"
-res = s.split(",")
-copy = res
-print(res)
-print(copy)
+# s = "hola,Agur,Hi,Gye,goodbye"
+# res = s.split(",")
+# copy = res
+# print(res)
+# print(copy)
 
-matriz = {}
-for i in res:
-    for j in copy:
-        matriz.update({j:i})
-print(matriz)
+# matriz = {}
+# for i in res:
+#     for j in copy:
+#         matriz.update({j:i})
+# print(matriz)
+
+
+# texto = "   programazioa gustatzen gustatzen   zait.  "
+# #print("X"+ texto.strip() + "X")
+# texto = "X"+ texto.replace("  ","") + "X"
+# print("X"+ texto.replace("  ","") + "X")
+# print(texto.count("gustatzen"))
+# y = len(texto)
+# print(texto[len(texto)-6:])
+# print(texto[-6:])
+
+
+# s="    122,Python,es,64,un,777,lenguaje,222,de,55,66,programación  "
+# s = s.strip()
+# x= s.split(",")
+# listaDePalabras = []
+# for i in x:
+#     if not i.isnumeric():
+#         print(i,"", end="")
+#         listaDePalabras.append(i)
+
+# resultado = " ".join(listaDePalabras)
+# print(resultado.upper())
+# print(resultado.title())
+
+
+# texto = "Pitón es un lenguaje de programación. Pitón está usado para la automación, análisis de datos e incluso la creación de páginas webs. Pitón fue creado por Bill Gates en 1960. Pitón es difícil de usar."
+# texto = texto.replace("Bill Gates", "Guido Van Rossum")
+# texto = texto.replace("difícil","facil").replace("Pitón","Python")
+# print(texto)
+
+
+texto = """    Lo más importante que nos ha mantenido en Python... bueno, hay 2 cosas 
+importantes. Uno son las bibliotecas. La otra cosa que nos mantiene en Python, y esto es lo más importante, es 
+facil de leer y entender. Cuando contratamos nuevos empleados. Solo digo, 
+'todo lo que escribas debe estar en python'. Sólo para que pueda leerlo. 
+Y es increíble porque puedo ver desde el otro lado de la habitación, mirando su pantalla, 
+si su código es bueno o malo. Porque un buen código de Python tiene una estructura muy obvia. 
+Y eso hace que mi vida sea mucho más fácil        """
+
+print(texto.upper().count("PYTHON"))
+print(texto.upper().find("PYTHON"))
+print(texto[texto.upper().find("PYTHON")+1:].upper().find("PYTHON"))
