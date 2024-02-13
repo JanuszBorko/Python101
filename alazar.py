bf08067f92b4054f31bc16ed5eb2d36f33b6db74
Janusz <januszborko@gmail.com>
2024-02-13 16:44:01 +0100

    ok

--- alazar.py
+++ alazar.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 # print(random.random())
 # print(random.randint(1,26))
@@ -14,21 +13,4 @@ print(lista)
 suma = 0
 for y in lista.values():
     suma = suma + y
-=======
-import random
-# print(random.random())
-# print(random.randint(1,26))
-
-lista = {"lunes":0,"martes":0,"miercoles":0,"jueves":0,"viernes":0}
-valores = list(lista.keys())
-print(valores)
-for x in range(100000):
-    dia = random.choice(valores)
-    contador = int(lista.get(dia)) +1
-    lista.update({dia:contador})
-print(lista)
-suma = 0
-for y in lista.values():
-    suma = suma + y
->>>>>>> ec36c0a444f02d26751d785df8e4b6626bedad40
 print(suma)
