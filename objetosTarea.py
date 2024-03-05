e34cd9970cf9ed300bcec35d025a1e8252610975
Janusz <januszborko@gmail.com>
2024-03-05 15:35:38 +0100

    OK

--- objetosTarea.py
+++ objetosTarea.py
@@ -12,30 +12,36 @@ class Programador(Empleado):
       self.lenguaje = lenguaje
 
     def calcular_nomina(self):
-       return self.nomina*1.2
+       if self.lenguaje == "JAVA":
+        return self.nomina*1.4
+       else:
+        return self.nomina*1.2
 
 
 class Sistemas_Nominas():
     def __init__(self, empleados) -> None:
        self.empleados = empleados
  
-    def calcular_nominas(self, empleados):
+    def calcular_nominas(self):
       print('Calculando nominas')
       print('___________________')
-      for empleado in empleados:
+      for empleado in self.empleados:
         print(f'Nomina para: {empleado.nombre} - {empleado.puesto}')
-        print(f'- $: {empleados.calcular_nomina(self)}')
+        print(f'- $: {empleado.calcular_nomina():.2f}')
         print('')
 
 
 # --------Main---------
 empleados = []        
 
-jon = Programador('Jon', 1200, 'Programador', 'Java')
+jon = Programador('Jon', 1200, 'Programador', 'JAVA')
+tom = Programador('Tom', 1000, 'Programador', 'php')
 ane = Empleado('Ane', 1300, 'Analista')
 
 empleados.append(jon)
+empleados.append(tom)
 empleados.append(ane)
 nominas = Sistemas_Nominas(empleados)
-nominas.calcular_nominas(empleados)
+nominas.calcular_nominas()
+
                
