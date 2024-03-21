8b9480df83b472e7e3ab70e0ecb551ee9e392935
Janusz <januszborko@gmail.com>
2024-03-21 17:50:41 +0100

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,27 +20,33 @@ import streamlit as st
 # if st.button("click me"):
 #     st.write(f"Tu edad  es {aplicarEdad(edad)}")
 
-left_column, right_column = st.columns(2)
-with left_column:
-    st.markdown("## CALCULADORA")
-    num1 = st.number_input("El primer numero", min_value=0, max_value=100)
-    num2 = st.number_input("El segundo numero:", min_value=0, max_value=100)
+# left_column, right_column = st.columns(2)
+# with left_column:
+#     st.markdown("## CALCULADORA")
+#     num1 = st.number_input("El primer numero", min_value=0, max_value=100)
+#     num2 = st.number_input("El segundo numero:", min_value=0, max_value=100)
 
-    oper = st.radio("Seleccionar operación: ", ["Sumar", "Multiplicar"])
-    match oper:
-        case "Sumar": resultado = num1 + num2
-        case "Multiplicar": resultado = num1 * num2
+#     oper = st.radio("Seleccionar operación: ", ["Sumar", "Multiplicar"])
+#     match oper:
+#         case "Sumar": resultado = num1 + num2
+#         case "Multiplicar": resultado = num1 * num2
 
-    if st.button("calcular"):
-        st.markdown(f"  # {resultado}")
+#     if st.button("calcular"):
+#         st.markdown(f"  # {resultado}")
 
 
-with right_column:
-    st.markdown("Tips")
-    st.video("https://www.youtube.com/watch?v=29mZql6xb9M")
+# with right_column:
+#     st.markdown("Tips")
+#     st.video("https://www.youtube.com/watch?v=29mZql6xb9M")
 
-add_slider = st.sidebar.slider('Select a range', 0.0, 100.0, (25.0, 75.0))
+# add_slider = st.sidebar.slider('Select a range', 0.0, 100.0, (25.0, 75.0))
 
-st.write(add_slider)
-#test
+# st.write(add_slider)
+# #test
+
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/sivabalanb/Data-Analysis-with-Pandas-and-Python/master/employees.csv")
+st.dataframe(df)
+st.balloons()
 
